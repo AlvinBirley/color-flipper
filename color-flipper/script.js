9a46b79a6fb34d87d4661674493f276df57e8808
AlvinBirley <alvinbirley@gmail.com>
2023-11-22 22:14:17 -0600

    Wire up div and button

---
+++ color-flipper/script.js
@@ -0,0 +1,24 @@
+var a;
+a=document.getElementById("p1")
+
+function flipper() {
+    console.log("It works!")
+}
+
+a.onclick=flipper
+
+var b;
+
+b=document.getElementById("p2")
+
+function hex() {
+    console.log("It also works!")
+}
+
+b.onclick=hex
+
+
+function buttonPress() {
+    console.log("Look at that button go!")
+}
+
