92e3fcc81db6bcbff722a9f5accd5b33c39d938a
AlvinBirley <alvinbirley@gmail.com>
2023-11-23 23:47:51 -0600

    Color flipper completed

--- color-flipper/script.js
+++ color-flipper/script.js
@@ -1,4 +1,4 @@
-let colorArray = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "gray", "aqua", "burlywood", "chartreuse", "darkgreen", "firebrck" ]
+let colorArray = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "gray", "aqua", "burlywood", "chartreuse", "darkgreen", "firebrick", "goldenrod", "hotpink", "indigo", "khaki", "lightcoral", "mediumaquamarine", "navajowhite", "olive", "plum", "royalblue", "rosybrown", "teal", "violet", "wheat", "yellowgreen",  ]
 
 const grabColor = getColor();
 
@@ -12,10 +12,11 @@ function buttonPress() {
    let grabColor = getColor();
     document.getElementById("bgc").innerHTML = grabColor;
     document.getElementById('bodcontainer').style.backgroundColor = grabColor;
+    document.getElementById('button').style.backgroundColor = grabColor;
    
 }
 
-buttonPress();
+
 
 
 
